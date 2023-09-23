6699b2a4bf667a9a5f3e536c71c3325c8a3f355c
br-bit3194 <bhavesh3194@gmail.com>
2023-09-23 22:58:56 +0530

    feat: total revenue added

--- 2.SQL-Essentials/6_cumulative_agg_and_ranking/OVER_PARTITION_BY.sql
+++ 2.SQL-Essentials/6_cumulative_agg_and_ranking/OVER_PARTITION_BY.sql
@@ -4,3 +4,9 @@ select to_char(dr.order_date::timestamp, 'yyyy-MM') AS order_month,
 	sum(order_revenue) over (partition by to_char(dr.order_date::timestamp, 'yyyy-MM')) AS monthly_order_revenue
 from daily_revenue AS dr
 order by 2;
+
+-- total aggregated revenue
+select dr.*,
+	sum(order_revenue) over (partition by 1) as total_order_revenue
+from daily_revenue as dr
+order by 1;
